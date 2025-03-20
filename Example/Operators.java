b1de301ac18c8986bb1acb4ea79c08c2f5848830
harshavardhan5335 <officialytmkbhd@gmail.com>
2025-03-20 15:47:39 +0530

    harsha update

--- Example/Operators.java
+++ Example/Operators.java
@@ -4,24 +4,22 @@ import java.util.Scanner;
 
 public class Operators {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in)
-            String name;
-            double price;
-            int quantity;
-            char currency = '$';
-        
-            
-        System.out.println("what is the name of the product : ");
-        name = sc.nextLine();
-        System.out.println("what is the price : ");
-        price = sc.nextDouble();
-        System.out.println("quantity : ");
-        quantity= sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        String name;
+        double price;
+        int quantity;
+        char currency = '$';
 
-        
-int a= (int)price;
+        System.out.print("what is the name of the product : ");
+        name = sc.nextLine();
+        System.out.print("what is the price : ");
+        price = sc.nextDouble();
+        System.out.print("quantity : ");
+        quantity = sc.nextInt();
+
+        int a = (int) price;
         int total = quantity * a;
-        System.out.println( "The total price for that items are " + total);
+        System.out.println("The total price for that items are " + total);
     }
 
 }
